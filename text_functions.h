5feaedd90225edaa6fd5b01c10ed899e95d77829
AdhamMahmoud1 <101528034+AdhamMahmoud1@users.noreply.github.com>
2022-05-15 20:39:27 +0200

    header file

---
+++ text_functions.h
@@ -0,0 +1,10 @@
+#ifndef TEXTNFUNCTIONS_H_INCLUDED
+#define TEXTNFUNCTIONS_H_INCLUDED
+using namespace std;
+
+void add();
+void display_content();
+void cls_file();
+void encrypt();
+void dencrypt();
+#endif
